6d6be199314aee52c52118a651b693fcca6ce611
Jack Delaney <jdelaney07@qub.ac.uk>
2023-08-17 17:14:09 +0100

    Trying to get ready for use

--- Sources/Sources/WordMayor.swift
+++ Sources/Sources/WordMayor.swift
@@ -1,15 +1,15 @@
 import Foundation
 
-
-public struct WordMayor {
-    public private(set) var text = ["Good Day, World!"]
-
-    public init() {
-        text = WordGetter.getWords(fileLocation: "autumn-one")
-        
-
-        
-        
-        
-    }
-}
+//
+//public struct WordMayor {
+//    public private(set) var text = ["Good Day, World!"]
+//
+//    public init() {
+//        text = WordGetter.getWords(fileLocation: "autumn-one")
+//
+//
+//
+//
+//
+//    }
+//}
